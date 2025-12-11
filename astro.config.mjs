d893ec5010b72326fcbb1e9ce4774391f24c7184
William Blacklock <BIGWilly@Joyces-MacBook-Air.local>
2025-12-11 13:54:46 -0600

    Fix Astro page rendering and add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: vercel(),
   integrations: [react(), tailwind()],
 
   vite: {
